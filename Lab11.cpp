0cec64bfed0abf38e2c75acc85e3f3e985615b76
llt3055 <llt30555@gmail.com>
2026-02-22 06:38:38 +0800

    create a struct and free the internal array

--- Lab11.cpp
+++ Lab11.cpp
@@ -1,1 +1,20 @@
 // COMSC-210 | Lab  11| Tianyi Cao
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//first create a struct include a dynamic array
+struct Athlete {
+    string name;
+    int number;
+    double *times;
+
+    //When the Athlete object is deleted, this frees the internal times array.
+    ~Athlete() {
+        if(times != nullptr) {
+            delete[] times;
+            times = nullptr;
+        }
+    }
+};
