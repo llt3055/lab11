2f72f3859183941bb8c005df119a70fe1b69bad3
llt3055 <llt30555@gmail.com>
2026-02-22 08:44:08 +0800

    debug: clear the buffer and add static variable in input function

--- Lab11.cpp
+++ Lab11.cpp
@@ -31,6 +31,8 @@ int main(){
     cin >> AthleteNum;
     cout << "Enter the number of times for each athlete: ";
     cin >> TimeNum;
+
+    cin.ignore();
     //create an array of Athlete objects
     Athlete *roster = new Athlete[AthleteNum];
     //for each athlete, create a dynamic array for times and get data
@@ -41,20 +43,26 @@ int main(){
     for (int i = 0; i < AthleteNum; i++) {
         showData(&roster[i], TimeNum);
     }
+    cin.ignore();
     //delete the roster array, which will also call the destructor for each Athlete
     delete[] roster;
     return 0;
 }
 
 void getData (Athlete *a, int num) {
+    static int nrStu = 1;
+    cout << "Input data for athlete #" << nrStu << ":\n";
     cout << "Enter name: ";
     getline(cin, a->name);
     cout << "Enter athlete number: ";
     cin >> a->number;
     for (int i = 0; i < num; i++) {
         cout << "Enter time " << i + 1 << ": ";
-        cin >> a->times[i];
+        cin >> a->times[i];      
     }
+    cin.ignore();
+    cout << endl << endl;
+    nrStu++;
 }
     
 void showData (Athlete *a, int num) {
@@ -62,5 +70,7 @@ void showData (Athlete *a, int num) {
     cout << "Athlete number: " << a->number << endl;
     for (int i = 0; i < num; i++) {
         cout << "Time " << i + 1 << ": " << a->times[i] << endl;
+        cout << endl << endl;
     }
+    
 }
