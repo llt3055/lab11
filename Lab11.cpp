5f7b7c4b6327c8e106a0bd2a655adac18f31a11d
llt3055 <llt30555@gmail.com>
2026-02-22 08:14:27 +0800

    implement getdata and showdata function in main and create a dynamic roster array

--- Lab11.cpp
+++ Lab11.cpp
@@ -31,6 +31,17 @@ int main(){
     cin >> AthleteNum;
     cout << "Enter the number of times for each athlete: ";
     cin >> TimeNum;
+    //create an array of Athlete objects
+    Athlete *roster = new Athlete[AthleteNum];
+    //for each athlete, create a dynamic array for times and get data
+    for (int i = 0; i < AthleteNum; i++) {
+        getData(&roster[i], TimeNum);
+    }
+    //show data for each athlete
+    for (int i = 0; i < AthleteNum; i++) {
+        showData(&roster[i], TimeNum);
+    }
+
 }
 
 void getData (Athlete *a, int num) {
@@ -38,9 +49,16 @@ void getData (Athlete *a, int num) {
     getline(cin, a->name);
     cout << "Enter athlete number: ";
     cin >> a->number;
+    for (int i = 0; i < num; i++) {
+        cout << "Enter time " << i + 1 << ": ";
+        cin >> a->times[i];
+    }
 }
     
 void showData (Athlete *a, int num) {
     cout << "Name: " << a->name << endl;
     cout << "Athlete number: " << a->number << endl;
+    for (int i = 0; i < num; i++) {
+        cout << "Time " << i + 1 << ": " << a->times[i] << endl;
+    }
 }
